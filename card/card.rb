9c9df50abdad46bf7f06846e5d5effbdb62c3fb2
tagirahmad <tagirbpl@gmail.com>
2021-07-01 21:50:57 +0300

    Create card

---
+++ card/card.rb
@@ -0,0 +1,21 @@
+class Card
+  attr_reader :name, :suit
+  attr_accessor :score
+
+  SUITS = %w[♠ ♥ ♣ ♦].freeze
+  LETTERS = %i[J Q K A].freeze
+  SCORES = { J: 10, Q: 10, K: 10, A: 11 }.freeze
+
+  def initialize(suit, name)
+    @suit = suit
+    @name = name
+  end
+
+  def price
+    @score = (2..10).include?(@name) ? @name : SCORES[@name]
+  end
+
+  def face
+    "#{@suit.to_s} #{@name.to_s}"
+  end
+end
