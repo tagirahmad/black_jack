07ffa7df6372a02e9a57afba3aaeba04b8903c3d
tagirahmad <tagirbpl@gmail.com>
2021-07-01 21:51:18 +0300

    Add card deck

---
+++ card/card_deck.rb
@@ -0,0 +1,17 @@
+class CardDeck
+  attr_reader :card_deck, :name
+
+  def initialize
+    @card_deck = []
+    generate_card_deck
+  end
+
+  def generate_card_deck
+    all_card_items = [(2..10).to_a, Card::LETTERS].flatten!
+    all_card_items.each do |item|
+      Card::SUITS.each do |suit|
+        @card_deck << Card.new(suit, item)
+      end
+    end
+  end
+end
